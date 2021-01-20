888cf88ee0e18484a7f3201f34da40aa22fdbe60
Victor Lucas <victorlucas.ao@gmail.com>
2021-01-20 00:23:06 -0400

    feat: add h2, mapper Categoria

--- src/main/java/com/victorlucas/cursomc/domain/Categoria.java
+++ src/main/java/com/victorlucas/cursomc/domain/Categoria.java
@@ -2,15 +2,21 @@ package com.victorlucas.cursomc.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
+@Entity
 public class Categoria implements Serializable {
     public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String nome;
 }
