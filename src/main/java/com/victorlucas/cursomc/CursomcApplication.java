70b4c65a45c31de188070ddead34d4edb805350e
Victor Lucas <victorlucas.ao@gmail.com>
2021-01-20 18:35:09 -0400

    feat: instantiation operation created

--- src/main/java/com/victorlucas/cursomc/CursomcApplication.java
+++ src/main/java/com/victorlucas/cursomc/CursomcApplication.java
@@ -1,12 +1,31 @@
 package com.victorlucas.cursomc;
 
+import com.victorlucas.cursomc.domain.Categoria;
+import com.victorlucas.cursomc.repositories.CategoriaRepository;
+import com.victorlucas.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
+
     }
 }
