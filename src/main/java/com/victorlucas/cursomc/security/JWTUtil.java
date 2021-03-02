30d3d277f27f0b8e112fd261ebf933b79535d8c9
victor lucas <victorlucas.ao@gmail.com>
2021-03-02 13:34:08 -0400

    fix: Error Authentication resolved

--- src/main/java/com/victorlucas/cursomc/security/JWTUtil.java
+++ src/main/java/com/victorlucas/cursomc/security/JWTUtil.java
@@ -1,6 +1,5 @@
 package com.victorlucas.cursomc.security;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,38 +21,7 @@ public class JWTUtil {
         return Jwts.builder() //É o cara que vai gerar o token
                 .setSubject(username) //É o usuário
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))//Tempo atual do sistema mais a expiração.
-                .signWith(SignatureAlgorithm.HS512, secret.getBytes()) // Isso é necessário para dizer como vai ser assinado o token. (O Algoritmo + segredo)
+                .signWith(SignatureAlgorithm.HS512, secret.getBytes(StandardCharsets.UTF_8)) // Isso é necessário para dizer como vai ser assinado o token. (O Algoritmo + segredo)
                 .compact(); //Para finalizar
     }
-
-    public boolean tokenValido(String token) {
-        Claims claims = getClaims(token);
-        if (claims != null){
-            String username = claims.getSubject();
-            Date expirationDate = claims.getExpiration();
-            Date now = new Date(System.currentTimeMillis());
-            if (username != null && expirationDate != null && now.before(expirationDate)/*Instânte atual ainda é anterior à expiração*/){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public String getUsername(String token) {
-        Claims claims = getClaims(token);
-        if (claims != null){
-            return claims.getSubject();
-        }
-        return null;
-    }
-
-    private Claims getClaims(String token) {
-        try {
-            return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
-        }catch (Exception ex){
-            return null;
-        }
-    }
-
-
 }
