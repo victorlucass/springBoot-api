d41d42052ef8716fa8ebf39012dfb0a477283808
victor lucas <victorlucas.ao@gmail.com>
2021-03-23 15:38:36 -0400

    feat: implementado a documentação do Swagger

--- src/main/java/com/victorlucas/cursomc/config/SecurityConfig.java
+++ src/main/java/com/victorlucas/cursomc/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -110,4 +111,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
     //Um método para codificar a senha.
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+        .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**");
+    }
 }
---
+++ src/main/java/com/victorlucas/cursomc/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.victorlucas.cursomc.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.victorlucas.cursomc.controllers"))
+				.paths(PathSelectors.any()).build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+		"API do curso Spring Boot",
+		"Esta API é utilizada no curso de Spring Boot do prof. Nelio Alves",
+		"Versão 1.0",
+		"https://www.udemy.com/terms",
+		new Contact("Victor Lucas", "https://www.linkedin.com/in/victor-lucas-52ba3b17a/", "victorlucas.ao@gmail.com"),
+		"Permitido uso para estudantes",
+		"https://www.udemy.com/terms",
+		Collections.emptyList() // Vendor Extensions
+	   );
+	   }
+}
